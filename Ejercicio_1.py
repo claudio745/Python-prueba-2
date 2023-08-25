ad5526bb63ba24b1f5f1dca3187389d7be7cd6aa
claudio afaro <74275767+claudio745@users.noreply.github.com>
2023-08-25 12:06:38 -0400

    a

---
+++ Ejercicio_1.py
@@ -0,0 +1,15 @@
+def primo(n):
+    if n % = 0:
+        print("El numero no es primo")
+        return False
+
+
+def main():
+    n = int(input("Ingrese un número para saber si es primo o no: "))
+    primo(n)
+    
+    
+    
+    
+
+
